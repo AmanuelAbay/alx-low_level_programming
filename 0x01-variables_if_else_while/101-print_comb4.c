d1ec4e60ee2cd08b0627fcdaf8fe52b264bf2ce7
AmanuelAbay <amanab.449@gmail.com>
2021-02-05 17:14:16 +0000

    advanced section

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,32 +5,36 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void){
-int x=0;
-int y=1;
-int z=2;
-while (x<=7)
+int main(void)
 {
-while(y<=8)
+int num1 = '0';
+int num2 = '1';
+int num3 = '2';
+while (num1 <= '7')
 {
-while(z<=9)
+while (num2 <= '8')
 {
-putchar(x);
-putchar(y);
-putchar(z);
-if (x != '7')
+while (num3 <= '9')
+{
+if (num2 != num3)
+{
+putchar(num1);
+putchar(num2);
+putchar(num3);
+if (num1 != '7')
 {
 putchar(',');
 putchar(' ');
 }
-z++;
 }
-y++;
-z=y;
+num3++;
 }
-x++;
-y=x;
+num2++;
+num3 = num2;
+}
+num1++;
+num2 = num1;
 }
 putchar('\n');
-return(0);
+return (0);
 }

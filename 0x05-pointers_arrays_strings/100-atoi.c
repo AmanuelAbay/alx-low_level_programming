ede46176747cb4a9e80d3aec43c5a59bfe3b223d
AmanuelAbay <amanab.449@gmail.com>
2021-02-17 14:56:08 +0000

    pointer with advanced section

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int sign = 1, i;
 unsigned int num = 0;
 for (i = 0; s[i]; i++)
 {
-if (s[i] == '-'){
+if (s[i] == '-')
 sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 num = (num * 10) + (s[i] - '0');
